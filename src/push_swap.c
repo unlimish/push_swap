220b69346f8d49d4678b89e10f3919c530d95cf7
unlimish <me@unlimi.sh>
2021-11-23 05:07:24 +0900

    💩 Add: push_swap.c It **doesn't** __meet the requirements__

---
+++ src/push_swap.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "push_swap.h"
+#include "libft.h"
+
+int	main(int ac, char **av)
+{
+	int **a;
+	int **b;
+
+	if (ac >= 2)
+		return (0);
+	av[0]="1";
+	check_is_int(ac, av);
+	while (*av != "\0")
+	{
+
+	}
+}
